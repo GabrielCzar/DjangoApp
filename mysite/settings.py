f5c0ac9ab30425cf78c830ed3bb62263733526d6
Gabriel Cesar <gcal1@live.com>
2016-09-11 00:57:24 -0300

    Configurando locais

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
 
 
 # Password validation
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -119,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
